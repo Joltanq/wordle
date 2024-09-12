78c496a17c468c1da08b7c45690b6bdfed2e9645
Joel Tan <joel.tan.92@gmail.com>
2024-09-13 00:29:15 +0800

    first commit

---
+++ jt_wordle_project.py
@@ -0,0 +1,11 @@
+def score_guess(target, guess):
+    score = [0,0,0,0,0]
+
+    if target == guess:
+        score = [2,2,2,2,2]
+    print(score)
+
+# Main
+target = "hello"
+guess = "world"
+score_guess(target,guess)
